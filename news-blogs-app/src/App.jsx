8faec69a5a89b10e954b7bcb4f7ba45130fc17c2
Aakarshk25 <aakarshkumar495@gmail.com>
2025-02-05 16:53:01 +0530

    Created Heaser , Body and Footer

--- news-blogs-app/src/App.jsx
+++ news-blogs-app/src/App.jsx
@@ -2,7 +2,11 @@
 import News from './Components/News'
 const  App =() => {
   return (
-    <div><News /></div>
+    <div className='container'>
+      <div className='news-blog-app'>
+        <News />
+      </div>
+      </div>
   )
 }
 
