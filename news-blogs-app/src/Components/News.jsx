784ad6037c0d034ecf778c15325243e324557f97
Aakarshk25 <aakarshkumar495@gmail.com>
2025-02-09 22:47:03 +0530

    Css completed

--- news-blogs-app/src/Components/News.jsx
+++ news-blogs-app/src/Components/News.jsx
@@ -1,6 +1,37 @@
-import  Weather  from "./Weather"
-import Calendar from "./Calendar"
-import './News.css'
+// import  Weather  from "./Weather"
+// import Calendar from "./Calendar"
+// import './News.css'
+// const News = () => {
+//   return (
+//     <div className="news">
+//       <header className="news-header">News Header</header>
+//       <div className="news-content">
+//         <div className="navbar"> 
+//           <div className="user">User</div>
+//           <nav className="categories">Categories</nav>
+//         </div>
+//         <div className="news-section">
+//           <div className="headline">Headline</div>
+//           <div className="news-grid">News Grid</div>
+//         </div>
+//         <div className="my-blogs">My Blogs</div>
+//         <div className="weather-calendar">
+//         <Weather />
+//         <Calendar />
+//         </div>
+
+//       </div>
+//       <footer className="news-footer">Footer</footer>
+//       </div>
+//   )
+// }
+
+// export default News
+
+import Weather from "./Weather";
+import Calendar from "./Calendar";
+import "./News.css";
+
 const News = () => {
   return (
     <div className="news">
@@ -16,14 +47,13 @@ const News = () => {
         </div>
         <div className="my-blogs">My Blogs</div>
         <div className="weather-calendar">
-        <Weather />
-        <Calendar />
+          <Weather />
+          <Calendar />
         </div>
-
       </div>
       <footer className="news-footer">Footer</footer>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default News
+export default News;
